e184e0219a4353273a8a50840ddc5b2bf914e8fd
alexandr polotovsky <apolotovsky@gmail.com>
2022-06-15 22:41:18 +0300

    Кубы чисел

---
+++ MathPow3/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Напишите программу, которая принимает на
+вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 4, 9.
+5 -> 1, 8, 27, 64, 125
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+ */
+
+ Console.WriteLine("Введите число N: ");
+ int N = int.Parse(Console.ReadLine());
+
+ int count = 1;
+ Console.Write("Кубы чисел от 1 до N: ");
+ while(count <= N)
+ {
+    Console.Write(Math.Pow(count, 3)  + " ");
+    count++;
+ }
